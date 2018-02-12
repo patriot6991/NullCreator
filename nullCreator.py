44adfe11abf92448db5655b6b687375fc0099901
patriot6991 <patriot6991@gmail.com>
2018-02-13 02:27:08 +0900

    refactor

--- nullCreator.py
+++ nullCreator.py
@@ -1,5 +1,6 @@
 import maya.cmds as mc
 
+
 class NullCreator(object):
     def __init__(self):
         self.pf = ''
@@ -12,34 +13,34 @@ class NullCreator(object):
         attr_temp = [0, 0, 0, 0, 0, 0, 1, 1, 1]
         attr_name = ['.tx', '.ty', '.tz', '.rx', '.ry', '.rz', '.sx', '.sy', '.sz']
 
-        #set name
+        # set name
         self.pf = str(mc.textField('f1', q=True, text=True))
         self.sf = str(mc.textField('f2', q=True, text=True))
         self.nn = str(mc.textField('f3', q=True, text=True))
 
-        #get some
+        # get some
         name_list = mc.ls(sl=True)
         parent_list = mc.listRelatives(parent=True)
 
         for i in range(9):
-            attr_temp[i] = mc.getAttr('%s%s' %(name_list[0], attr_name[i]))
+            attr_temp[i] = mc.getAttr('%s%s' % (name_list[0], attr_name[i]))
 
-        #new name
+        # new name
         if self.j == 0:
             node_name = '%s%s%s' % (self.pf, name_list[0], self.sf)
         else:
             node_name = self.nn
 
-        #create node
+        # create node
         mc.createNode('transform', n=node_name)
         mc.parent(name_list[0], node_name)
         if parent_list is not None and any(parent_list):
             mc.parent(node_name, parent_list[0])
 
-        #setAttr
+        # setAttr
         for i in range(9):
-            mc.setAttr('%s%s' %(name_list[0], attr_name[i]), attr_list[i])
-            mc.setAttr('%s%s' %(node_name, attr_name[i]), attr_temp[i])
+            mc.setAttr('%s%s' % (name_list[0], attr_name[i]), attr_list[i])
+            mc.setAttr('%s%s' % (node_name, attr_name[i]), attr_temp[i])
 
     def original(self, *args):
         mc.text('t1', en=True, edit=True)
@@ -59,15 +60,15 @@ class NullCreator(object):
         mc.textField('f3', en=True, edit=True)
         self.j = 1
 
-    def ui(self):
-        win = mc.window(title='nullCreator', widthHeight=(300,170))
+    def ui(self, *args):
+        win = mc.window(title='nullCreator', widthHeight=(300, 170))
         form = mc.formLayout()
 
-        rc = mc.radioCollection()
+        mc.radioCollection()
         rb1 = mc.radioButton(l='Original Name', sl=True, onc=self.original)
         rb2 = mc.radioButton(l='New Name', onc=self.new)
-        t1 = mc.text('t1', l='Prefix :', w=50,  h=20, al='right', en=True)
-        t2 = mc.text('t2', l='Suffix :', w=50,  h=20, al='right', en=True)
+        t1 = mc.text('t1', l='Prefix :', w=50, h=20, al='right', en=True)
+        t2 = mc.text('t2', l='Suffix :', w=50, h=20, al='right', en=True)
         t3 = mc.text('t3', l='Name :', w=50, h=20, al='right', en=False)
         f1 = mc.textField('f1', w=225, h=20, text=self.pf, en=True)
         f2 = mc.textField('f2', w=225, h=20, text=self.sf, en=True)
@@ -76,19 +77,20 @@ class NullCreator(object):
         b1 = mc.button(l='Create', w=280, h=40, c=self.create)
 
         mc.formLayout(form, edit=True, attachForm=[
-            (rb1,'top',10),(rb1,'left',30),
+            (rb1, 'top', 10), (rb1, 'left', 30),
             (rb2, 'top', 10), (rb2, 'left', 160),
-            (t1,'top',35),(t1,'left',10),
-            (t2,'top',60),(t2,'left',10),
-            (t3,'top',90),(t3,'left',10),
-            (f1,'top',35),(f1,'left',65),
-            (f2,'top',60),(f2,'left',65),
-            (f3,'top',90),(f3,'left',65),
-            (sp,'top',85),(sp,'left',10),
-            (b1,'top',120),(b1,'left',10)
+            (t1, 'top', 35), (t1, 'left', 10),
+            (t2, 'top', 60), (t2, 'left', 10),
+            (t3, 'top', 90), (t3, 'left', 10),
+            (f1, 'top', 35), (f1, 'left', 65),
+            (f2, 'top', 60), (f2, 'left', 65),
+            (f3, 'top', 90), (f3, 'left', 65),
+            (sp, 'top', 85), (sp, 'left', 10),
+            (b1, 'top', 120), (b1, 'left', 10)
         ])
 
         mc.showWindow(win)
+
 
 a = NullCreator()
 a.ui()
